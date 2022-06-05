ec3ea0353afabd3b964b35ed42d0ce74b9273452
Sergiu Vidreanu <36509217+vidrez@users.noreply.github.com>
2022-06-05 12:38:31 +0200

    Delete autostart.sh

--- autostart.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-CWD=$(pwd)
-sudo screen -AmdS mserver java -Xmx1024M -Xms1024M -jar "${CWD}/setup/minercfat-server/server.jar" nogui
-sudo screen -AmdS dockerpoc docker run --network host log4j-shell-poc
