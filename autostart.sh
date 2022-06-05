145f9df55aa45163886b0e5a2441c29ef222d3e8
Sergiu Vidreanu <36509217+vidrez@users.noreply.github.com>
2022-06-05 11:49:45 +0200

    Update autostart.sh

--- autostart.sh
+++ autostart.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-screen -AmdS mserver java -Xmx1024M -Xms1024M -jar ./minercfat-server/server.jar nogui
-screen -AmdS dockerpoc docker run --network host log4j-shell-poc
+CWD=$(pwd)
+sudo screen -AmdS mserver java -Xmx1024M -Xms1024M -jar "${CWD}/setup/minercfat-server/server.jar" nogui
+sudo screen -AmdS dockerpoc docker run --network host log4j-shell-poc
