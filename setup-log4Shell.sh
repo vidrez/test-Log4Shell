c954de363fd67208b45739a583fd9b28d84258b6
Sergiu Vidreanu <36509217+vidrez@users.noreply.github.com>
2022-06-05 12:12:22 +0200

    Update setup-log4Shell.sh

--- setup-log4Shell.sh
+++ setup-log4Shell.sh
@@ -47,7 +47,7 @@ screen -AmdS dockerpoc docker run --network host log4j-shell-poc
 touch autostart.sh
 echo "#!/bin/bash" >> autostart.sh
 echo "screen -AmdS mserver java -Xmx1024M -Xms1024M -jar '$(pwd)/minercfat-server/server.jar' nogui" >> autostart.sh
-ehco "screen -AmdS dockerpoc docker run --network host log4j-shell-poc" >> autostart.sh
+echo "screen -AmdS dockerpoc docker run --network host log4j-shell-poc" >> autostart.sh
 chmod +x autostart.sh
 
 echo " "
